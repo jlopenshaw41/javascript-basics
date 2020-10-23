2c2125ce28465b096a45b3a5b2c65e2445434fd9
Jennifer Cant <jcant6@gmail.com>
2020-10-23 13:11:47 +0100

    numbers test complete

--- src/numbers.js
+++ src/numbers.js
@@ -1,46 +1,31 @@
-const add = (a, b) => {
-  // your code here
-};
+const add = (a, b) => a + b;
 
-const subtract = (a, b) => {
-  // your code here
-};
 
-const multiply = (a, b) => {
-  // your code here
-};
+const subtract = (a, b) => a - b;
 
-const divide = (a, b) => {
-  // your code here
-};
+const multiply = (a, b) => a * b;
 
-const power = (a, b) => {
-  // your code here
-};
+const divide = (a, b) => a / b;
 
-const round = a => {
-  // your code here
-};
+const power = (a, b) => Math.pow(a, b);
 
-const roundUp = a => {
-  // your code here
-};
+const round = a => Math.round(a);
 
-const roundDown = a => {
-  // your code here
-};
+const roundUp = a => Math.ceil(a);
 
-const absolute = a => {
-  // your code here
-};
+const roundDown = a => Math.floor(a);
+
+const absolute = a => Math.abs(a);
 
 const quotient = (a, b) => {
-  // your code here
+
+  const remainder = a % b;
+  const newA = a - remainder;
+  return newA / b;
+
 };
 
-const remainder = (a, b) => {
-  // your code here
-};
+const remainder = (a, b) => a % b;
 
 module.exports = {
   add,
